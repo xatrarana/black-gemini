1bb1a5ff4780fa079e52c772d79c217c65a69050
xatrarana <64579978+xatrarana@users.noreply.github.com>
2020-05-31 10:39:17 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login page</title>
+	<link rel="stylesheet" type="text/css" href="style4.css">
+</head>
+<body>
+
+    <div id="frm">
+    	<form action="process.php" method="POST">
+    		<p>
+    			<label>Username:</label>
+    			<input type="text" name="user" id="user"/>
+    			</p>
+    		<p>
+    			<label>Password:</label>
+    			<input type="password" name="pass" id="pass"/>
+    			</p>
+    		<p>
+    			<input type="Submit" value="login" id="btn"/>
+    			</p>
+                
+    	</form>
+    </div> 
+
+</body>
+</html>
